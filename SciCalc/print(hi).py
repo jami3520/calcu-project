37dc33b1082f369b3ab1e64dc8255bdc73940580
jami3520 <mdj99589@gmail.com>
2026-02-26 19:46:38 +0530

    Add files via upload

---
+++ SciCalc/print(hi).py
@@ -0,0 +1,14 @@
+# #dictionary
+# js = {"name":"jamy", "age": 20, "city": "islampur"}
+# print(js.keys())
+# print(js.values())
+# print(js.items())
+
+#tuple data type
+
+tuple = ('1,2,3,4,5 ', 'hello')
+tuple1 = ('hello', 'world')
+print(tuple1[0])
+print(tuple[0])
+print(tuple[1])
+print(tuple + tuple1)
